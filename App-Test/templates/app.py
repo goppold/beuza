32a8ee4094acd784a9a834865d58015a0ac04744
Sebastian <sebastian2.bock@oth-regensburg.de>
2020-02-11 09:00:23 +0100

    Basic Navigation

---
+++ App-Test/templates/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
+if __name__ == '__main__':
+    app.run()
